059778e63647ab970f9caf72de953ad3b168347d
rocmc <rocmc@naver.com>
2017-12-02 15:33:09 +0900

    init

---
+++ sample.py
@@ -0,0 +1,1 @@
+print("i={0} f={1} s={2} s1={3}".format(123, 1.14159, 'python', "python-1"))
---
+++ test.py
@@ -0,0 +1,8 @@
+﻿#coding: utf-8
+#!/user/bin/env python
+#helloworld.py
+print ("sss")
+print("xxx")
+print ()
+print("기레기")
+
